9f29ae5342b7eea91d76b70557ee9fbcef9a91cb
443460518@qq.com <443460518@qq.com>
2016-06-16 11:22:12 +0800

    11

---
+++ Test_git/Test_git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_git
+//
+//  Created by rui_james on 16/6/16.
+//  Copyright © 2016年 rui_james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test_git/Test_git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test_git
+//
+//  Created by rui_james on 16/6/16.
+//  Copyright © 2016年 rui_james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
